4e93daecd82eb1e3002557f8e352d48aca6a5002
eLuckyan <peluckyan@gmail.com>
2024-08-24 03:50:44 +0530

    about content UI

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.scss";
+import content1 from "../../Assets/Images/1088523.jpg"
 
 const Home = () => {
   return (
@@ -10,7 +11,22 @@ const Home = () => {
         </div>
         <div className="about-content">
           <div className="content1">
-            
+            <div className="content-header">
+              <img src={content1} alt="" className="image-container" />
+              <h3>Infrastructure</h3>
+            </div>
+            <div className="content-content">
+              <h5>
+              Well designed infrastructure with sophisticated needs to cater your professionals
+              </h5>
+              <br />
+              <p>
+              We value the space required to provide hands on training to budding Agricultural Professionals. We manifest a pattern...
+              </p>
+              <button>
+                Read more..
+              </button>
+            </div>
           </div>
         </div>
       </div>
