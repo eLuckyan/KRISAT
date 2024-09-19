bf27235a56425e726a69273b85a9a3a8df0e0184
Jayasurya260528 <jayasurya260528@gmail.com>
2024-09-20 01:53:09 +0530

    Photo gallery changes

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.scss";
+import PhotoGallery from "../../Components/Gallery/PhotoGallery";
 
 const Home = () => {
   return (
     <>
       <h2>Home</h2>
+      <PhotoGallery />
     </>
   );
 };
